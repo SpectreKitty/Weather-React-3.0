7f4071b9bf192f32fd342df667e0440f55e8d588
SpectreKitty <astry.brana@gmail.com>
2024-01-24 10:35:02 -0500

    created structure for site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather.js";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Weather />
         <footer>
           Code by Astry Brana, open sourced on{" "}
           <a href="https://github.com/SpectreKitty/Weather-React-3.0">GitHub</a>{" "}
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter City"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>{" "}
+        </div>
+      </form>
+      <h2>Miami</h2>
+      <ul>
+        <li>Wednesday 10:24AM</li>
+        <li>Partly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly cloudy"
+          />
+          78°F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipation: 30%</li>
+            <li>Humidity: 89%</li>
+            <li>Wind: 5mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
