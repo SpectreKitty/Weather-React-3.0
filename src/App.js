6e6570e33d9227bd88a800e653f2080a712740dd
SpectreKitty <astry.brana@gmail.com>
2024-01-24 10:14:33 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Code by Astry Brana, open sourced on{" "}
-        <a href="https://github.com/SpectreKitty/Weather-React-3.0">GitHub</a>{" "}
-        and hosted on <a href="https://weatherreact3.netlify.app/"> Netlify</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Code by Astry Brana, open sourced on{" "}
+          <a href="https://github.com/SpectreKitty/Weather-React-3.0">GitHub</a>{" "}
+          and hosted on{" "}
+          <a href="https://weatherreact3.netlify.app/"> Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
